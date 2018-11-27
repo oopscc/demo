26817c29af63cf9198bf39efb51cddbb3e0435ba
锄木 <lwq96439@alibaba-inc.com>
2018-11-27 23:31:59 +0800

    init

---
+++ src/demo/promise.js
@@ -0,0 +1,14 @@
+export default class MyPromise {
+    constructor(fn) {
+        console.log("promise")
+        this.successList = [];
+        this.failList = [];
+        // fn()
+    }
+    then(successCallback, failCallback) {
+
+    }
+    catch() {
+
+    }
+}
